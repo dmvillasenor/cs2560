3c63ba4badf7fe1198551bb8b9285fc82800d4ff
Darrell Villasenor <dmvillasenor@cpp.edu>
2019-02-14 14:57:38 -0800

    Finish Restaurant Program

--- Homework 2/ResturantBill.c
+++ Homework 2/ResturantBill.c
@@ -7,8 +7,15 @@ int main() {
 	double tax = 0.0675;
 	double tipPercent = 0.2;
 
+	printf("Meal Cost: %.2f\n", bill);
+	printf("Tax: %0.2f\n", tax*bill);
+
 	bill += tax*bill;
+
+	printf("Tip Amount: %.2f\n", tipPercent*bill);
+
 	bill += tipPercent*bill;
+
 
 	printf("Total: $%.2f", bill);
 
