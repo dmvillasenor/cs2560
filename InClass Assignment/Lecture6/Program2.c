d994f914a66b1f0e2f1f103d4653c636c0a9b1e9
Darrell Villasenor <dmvillasenor@cpp.edu>
2019-02-19 18:12:53 -0800

    InClass Code

---
+++ InClass Assignment/Lecture6/Program2.c
@@ -0,0 +1,18 @@
+/*
+ 2*/
+#include <stdio.h>  //standard input output built into c library
+
+typedef int number;
+
+ int main() {
+
+   number c;
+   printf( "Enter a value :");
+   //fflush(stdout);  //ECLIPSE BUG
+   c = getchar( );
+
+   printf( "\nYou entered: ");
+   putchar( c );
+
+   return 0;
+}
